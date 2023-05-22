1a942c47cf28e98275e375bc2103c775b49dc281
Denis Rosa <denisrosa@MacBook-Pro-3.local>
2023-05-22 09:33:45 +0200

    adding remaining code completion sugestions

--- src/main/java/org/intellij/sdk/language/SimpleCompletionContributor.java
+++ src/main/java/org/intellij/sdk/language/SimpleCompletionContributor.java
@@ -1,14 +1,10 @@
-// Copyright 2000-2022 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
 package org.intellij.sdk.language;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.openapi.util.io.FileAttributes;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.util.ProcessingContext;
-import generated.GeneratedTypes;
 import org.jetbrains.annotations.NotNull;
 
 public class SimpleCompletionContributor extends CompletionContributor {
