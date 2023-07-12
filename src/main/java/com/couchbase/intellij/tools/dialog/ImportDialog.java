2f27232dc2bba0fc265c438a6d94a112822cf6d8
teetangh <teetangh@gmail.com>
2023-07-12 09:26:25 +0530

    added document listeners + key previews + fixed button text + updated Summary preview html

--- src/main/java/com/couchbase/intellij/tools/dialog/ImportDialog.java
+++ src/main/java/com/couchbase/intellij/tools/dialog/ImportDialog.java
@@ -7,12 +7,13 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.Set;
 
 import javax.swing.Action;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -74,12 +75,24 @@ public class ImportDialog extends DialogWrapper {
     private JPanel cardPanel;
     private JPanel keyPreviewPanel;
 
-    private JButton backButton;
-    private JButton nextButton;
-
     private int currentPage;
 
+    private JBLabel scopeLabel;
+    private JBLabel collectionLabel;
+    private JBLabel scopeFieldLabel;
+    private JBLabel collectionFieldLabel;
+    private JBLabel keyLabel;
+    private JBLabel fieldNameLabel;
+    private JBLabel expressionLabel;
+    private JBLabel skipFirstLabel;
+    private JBLabel importUptoLabel;
+    private JBLabel ignoreFieldsLabel;
+    private JBLabel threadsLabel;
+    private JBLabel verboseLabel;
     private JBLabel summaryLabel;
+
+    private Action backAction;
+    private Action nextAction;
 
     public ImportDialog() {
         super(true);
@@ -96,9 +109,6 @@ public class ImportDialog extends DialogWrapper {
         // Initialize additional components for navigation and summary
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
-
-        backButton = new JButton("Back");
-        nextButton = new JButton("Next");
 
         currentPage = 1;
 
@@ -181,7 +191,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 2;
         c.weightx = 0.3;
         c.gridx = 0;
-        targetFormPanel.add(new JBLabel("Scope:"), c);
+        scopeLabel = new JBLabel("Scope:");
+        targetFormPanel.add(scopeLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -191,7 +202,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 3;
         c.weightx = 0.3;
         c.gridx = 0;
-        targetFormPanel.add(new JBLabel("Collection:"), c);
+        collectionLabel = new JBLabel("Collection:");
+        targetFormPanel.add(collectionLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -202,7 +214,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 4;
         c.weightx = 0.3;
         c.gridx = 0;
-        targetFormPanel.add(new JBLabel("Scope Field:"), c);
+        scopeFieldLabel = new JBLabel("Scope Field:");
+        targetFormPanel.add(scopeFieldLabel, c);
 
         c.weightx = 0.7;
         c.gridx = 1;
@@ -213,12 +226,35 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 5;
         c.weightx = 0.3;
         c.gridx = 0;
-        targetFormPanel.add(new JBLabel("Collection Field:"), c);
+        collectionFieldLabel = new JBLabel("Collection Field:");
+        targetFormPanel.add(collectionFieldLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
         collectionFieldField = new JBTextField();
         targetFormPanel.add(collectionFieldField, c);
+
+        // Set all labels to invisible and disabled by default
+        scopeLabel.setVisible(false);
+        collectionLabel.setVisible(false);
+        scopeFieldLabel.setVisible(false);
+        collectionFieldLabel.setVisible(false);
+
+        scopeLabel.setEnabled(false);
+        collectionLabel.setEnabled(false);
+        scopeFieldLabel.setEnabled(false);
+        collectionFieldLabel.setEnabled(false);
+
+        // Set all fields to invisible and disabled by default
+        scopeCombo.setVisible(false);
+        collectionCombo.setVisible(false);
+        scopeFieldField.setVisible(false);
+        collectionFieldField.setVisible(false);
+
+        scopeCombo.setEnabled(false);
+        collectionCombo.setEnabled(false);
+        scopeFieldField.setEnabled(false);
+        collectionFieldField.setEnabled(false);
 
         targetPanel.add(targetFormPanel, BorderLayout.CENTER);
 
@@ -234,7 +270,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 0;
         c.weightx = 0.3;
         c.gridx = 0;
-        keyFormPanel.add(new JBLabel("Key Options:"), c);
+        keyLabel = new JBLabel("Key Options:");
+        keyFormPanel.add(keyLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -260,7 +297,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 1;
         c.weightx = 0.3;
         c.gridx = 0;
-        keyFormPanel.add(new JBLabel("Field Name:"), c);
+        fieldNameLabel = new JBLabel("Field Name:");
+        keyFormPanel.add(fieldNameLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -271,7 +309,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 2;
         c.weightx = 0.3;
         c.gridx = 0;
-        keyFormPanel.add(new JBLabel("Expression:"), c);
+        expressionLabel = new JBLabel("Expression:");
+        keyFormPanel.add(expressionLabel, c);
 
         c.weightx = 0.7;
         c.gridx = 1;
@@ -296,6 +335,22 @@ public class ImportDialog extends DialogWrapper {
             }
         });
 
+        // Set all labels to invisible and disabled by default
+        fieldNameLabel.setVisible(false);
+        expressionLabel.setVisible(false);
+        fieldNameLabel.setEnabled(false);
+        expressionLabel.setEnabled(false);
+
+        // Set all fields to invisible and disabled by default
+        fieldNameField.setVisible(false);
+        expressionField.setVisible(false);
+        fieldNameField.setEnabled(false);
+        expressionField.setEnabled(false);
+
+        // Set the preview panel to invisible and disabled by default
+        keyPreviewPanel.setVisible(false);
+        keyPreviewPanel.setEnabled(false);
+
         keyPanel.add(keyFormPanel, BorderLayout.CENTER);
 
         // Page 4: Advanced options
@@ -310,7 +365,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 0;
         c.weightx = 0.3;
         c.gridx = 0;
-        advancedFormPanel.add(new JBLabel("Skip the first # Documents:"), c);
+        skipFirstLabel = new JBLabel("Skip the first # Documents:");
+        advancedFormPanel.add(skipFirstLabel, c);
         c.weightx = 0.5;
         c.gridx = 1;
 
@@ -327,7 +383,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 1;
         c.weightx = 0.3;
         c.gridx = 0;
-        advancedFormPanel.add(new JBLabel("Import up to # Documents:"), c);
+        importUptoLabel = new JBLabel("Import up to # Documents:");
+        advancedFormPanel.add(importUptoLabel, c);
         c.weightx = 0.5;
         c.gridx = 1;
 
@@ -343,7 +400,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 2;
         c.weightx = 0.3;
         c.gridx = 0;
-        advancedFormPanel.add(new JBLabel("Ignore the fields:"), c);
+        ignoreFieldsLabel = new JBLabel("Ignore the fields:");
+        advancedFormPanel.add(ignoreFieldsLabel, c);
         c.weightx = 0.5;
         c.gridx = 1;
 
@@ -360,7 +418,9 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 3;
         c.weightx = 0.3;
         c.gridx = 0;
-        advancedFormPanel.add(new JBLabel("Threads:"), c);
+        threadsLabel = new JBLabel("Threads:");
+        advancedFormPanel.add(threadsLabel, c);
+
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -371,7 +431,8 @@ public class ImportDialog extends DialogWrapper {
         c.gridy = 4;
         c.weightx = 0.3;
         c.gridx = 0;
-        advancedFormPanel.add(new JBLabel("Verbose Log:"), c);
+        verboseLabel = new JBLabel("Verbose Log:");
+        advancedFormPanel.add(verboseLabel, c);
         c.weightx = 0.7;
         c.gridx = 1;
 
@@ -385,8 +446,9 @@ public class ImportDialog extends DialogWrapper {
         summaryPanel.add(new TitledSeparator("Summary"), BorderLayout.NORTH);
 
         summaryLabel = new JBLabel();
-
         summaryPanel.add(summaryLabel, BorderLayout.CENTER);
+
+        updateSummary();
 
         // Add pages to card panel
         cardPanel.add(datasetPanel, "1");
@@ -397,18 +459,6 @@ public class ImportDialog extends DialogWrapper {
 
         mainPanel.add(cardPanel, BorderLayout.CENTER);
 
-        // Add navigation buttons
-        // JPanel buttonPanel = new JPanel();
-        // backButton.setEnabled(false);
-        // backButton.addActionListener(e -> previousPage());
-        // buttonPanel.add(backButton);
-
-        // nextButton.addActionListener(e -> nextPage());
-        // buttonPanel.add(nextButton);
-
-        // mainPanel.add(buttonPanel, BorderLayout.SOUTH);
-
-        // Add listeners to update summary and show/hide fields
         addListeners();
 
         return mainPanel;
@@ -419,30 +469,128 @@ public class ImportDialog extends DialogWrapper {
         defaultScopeAndCollectionRadio.addActionListener(e -> updateScopeAndCollectionFields());
         collectionRadio.addActionListener(e -> updateScopeAndCollectionFields());
         dynamicScopeAndCollectionRadio.addActionListener(e -> updateScopeAndCollectionFields());
+
+        // Add listeners for Page 3 radio buttons
+        generateUUIDRadio.addActionListener(e -> updateKeyFormFields());
+        useFieldValueRadio.addActionListener(e -> updateKeyFormFields());
+        customExpressionRadio.addActionListener(e -> updateKeyFormFields());
+
+        DocumentAdapter updateSummaryListener = new DocumentAdapter() {
+            @Override
+            protected void textChanged(DocumentEvent e) {
+                updateSummary();
+            }
+        };
+
+        // Add listeners for Page 1 fields
+        // Add listener for datasetField
+        datasetField.getTextField().getDocument().addDocumentListener(updateSummaryListener);
+        // Add listeners for Page 2 fields
+        defaultScopeAndCollectionRadio.addActionListener(e -> updateSummary());
+        collectionRadio.addActionListener(e -> updateSummary());
+        dynamicScopeAndCollectionRadio.addActionListener(e -> updateSummary());
+
+        scopeCombo.addActionListener(e -> updateSummary());
+        collectionCombo.addActionListener(e -> updateSummary());
+
+        scopeFieldField.getDocument().addDocumentListener(updateSummaryListener);
+        collectionFieldField.getDocument().addDocumentListener(updateSummaryListener);
+
+        // Add listeners for Page 3 fields
+        generateUUIDRadio.addActionListener(e -> updateSummary());
+        useFieldValueRadio.addActionListener(e -> updateSummary());
+        customExpressionRadio.addActionListener(e -> updateSummary());
+
+        fieldNameField.getDocument().addDocumentListener(updateSummaryListener);
+        expressionField.getDocument().addDocumentListener(updateSummaryListener);
+
+        // Add listeners for Page 4 fields
+        skipFirstField.getDocument().addDocumentListener(updateSummaryListener);
+        importUptoField.getDocument().addDocumentListener(updateSummaryListener);
+        ignoreFieldsField.getDocument().addDocumentListener(updateSummaryListener);
+
+        skipFirstCheck.addActionListener(e -> updateSummary());
+        importUptoCheck.addActionListener(e -> updateSummary());
+        ignoreFieldsCheck.addActionListener(e -> updateSummary());
+
+        threadsField.getDocument().addDocumentListener(updateSummaryListener);
+        verboseCheck.addActionListener(e -> updateSummary());
+
+        // Add listener for Page 5
+        summaryLabel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentShown(ComponentEvent e) {
+                updateSummary();
+            }
+        });
+
     }
 
     private void updateScopeAndCollectionFields() {
         boolean collectionSelected = collectionRadio.isSelected();
+
+        scopeLabel.setVisible(collectionSelected);
         scopeCombo.setVisible(collectionSelected);
+        scopeLabel.setEnabled(collectionSelected);
+        scopeCombo.setEnabled(collectionSelected);
+
+        collectionLabel.setVisible(collectionSelected);
         collectionCombo.setVisible(collectionSelected);
+        collectionLabel.setEnabled(collectionSelected);
+        collectionCombo.setEnabled(collectionSelected);
 
         boolean dynamicSelected = dynamicScopeAndCollectionRadio.isSelected();
+
+        scopeFieldLabel.setVisible(dynamicSelected);
         scopeFieldField.setVisible(dynamicSelected);
+        scopeFieldLabel.setEnabled(dynamicSelected);
+        scopeFieldField.setEnabled(dynamicSelected);
+
+        collectionFieldLabel.setVisible(dynamicSelected);
         collectionFieldField.setVisible(dynamicSelected);
+        collectionFieldLabel.setEnabled(dynamicSelected);
+        collectionFieldField.setEnabled(dynamicSelected);
+
+        updateSummary();
+    }
+
+    private void updateKeyFormFields() {
+        boolean useFieldValueSelected = useFieldValueRadio.isSelected();
+
+        fieldNameLabel.setVisible(useFieldValueSelected);
+        fieldNameField.setVisible(useFieldValueSelected);
+        fieldNameLabel.setEnabled(useFieldValueSelected);
+        fieldNameField.setEnabled(useFieldValueSelected);
+
+        boolean customExpressionSelected = customExpressionRadio.isSelected();
+
+        expressionLabel.setVisible(customExpressionSelected);
+        expressionField.setVisible(customExpressionSelected);
+        expressionLabel.setEnabled(customExpressionSelected);
+        expressionField.setEnabled(customExpressionSelected);
+
+        boolean keyPreviewVisible = useFieldValueSelected || customExpressionSelected;
+
+        keyPreviewPanel.setVisible(keyPreviewVisible);
+        keyPreviewPanel.setEnabled(keyPreviewVisible);
+
+        updateSummary();
     }
 
     private void updateKeyPreview() {
-        // Update key preview panel with preview content
+        // Clear existing preview content
         keyPreviewPanel.removeAll();
 
         if (useFieldValueRadio.isSelected()) {
             // Generate preview based on field name
             String fieldName = fieldNameField.getText();
             // Add preview content to keyPreviewPanel
+            keyPreviewPanel.add(new JBLabel("Preview: " + fieldName));
         } else if (customExpressionRadio.isSelected()) {
             // Generate preview based on custom expression
             String expression = expressionField.getText();
             // Add preview content to keyPreviewPanel
+            keyPreviewPanel.add(new JBLabel("Preview: " + expression));
         }
 
         keyPreviewPanel.revalidate();
@@ -521,12 +669,31 @@ public class ImportDialog extends DialogWrapper {
         summaryLabel.setText(summary.toString());
     }
 
+    @Override
+    protected Action @NotNull [] createActions() {
+        Action cancelAction = getCancelAction();
+        backAction = new DialogWrapperAction("Back") {
+            @Override
+            protected void doAction(ActionEvent e) {
+                previousPage();
+            }
+        };
+        nextAction = new DialogWrapperAction("Next") {
+            @Override
+            protected void doAction(ActionEvent e) {
+                nextPage();
+            }
+        };
+
+        return new Action[] { cancelAction, backAction, nextAction };
+    }
+
     private void previousPage() {
         if (currentPage > 1) {
             currentPage--;
             cardLayout.show(cardPanel, Integer.toString(currentPage));
-            backButton.setEnabled(currentPage > 1);
-            nextButton.setText(currentPage == 5 ? "Import" : "Next");
+            backAction.putValue(Action.NAME, currentPage == 1 ? "" : "Back");
+            nextAction.putValue(Action.NAME, currentPage == 5 ? "Import" : "Next");
         }
     }
 
@@ -534,8 +701,8 @@ public class ImportDialog extends DialogWrapper {
         if (currentPage < 5) {
             currentPage++;
             cardLayout.show(cardPanel, Integer.toString(currentPage));
-            backButton.setEnabled(true);
-            nextButton.setText(currentPage == 5 ? "Import" : "Next");
+            backAction.putValue(Action.NAME, currentPage == 1 ? "" : "Back");
+            nextAction.putValue(Action.NAME, currentPage == 5 ? "Import" : "Next");
         } else {
             doOKAction();
         }
@@ -547,23 +714,4 @@ public class ImportDialog extends DialogWrapper {
         super.doOKAction();
     }
 
-    @Override
-    protected Action @NotNull [] createActions() {
-        Action cancelAction = getCancelAction();
-        Action backAction = new DialogWrapperAction("Back") {
-            @Override
-            protected void doAction(ActionEvent e) {
-                previousPage();
-            }
-        };
-        Action nextAction = new DialogWrapperAction(currentPage == 5 ? "Import" : "Next") {
-            @Override
-            protected void doAction(ActionEvent e) {
-                nextPage();
-            }
-
-        };
-
-        return new Action[] { cancelAction, backAction, nextAction };
-    }
 }
