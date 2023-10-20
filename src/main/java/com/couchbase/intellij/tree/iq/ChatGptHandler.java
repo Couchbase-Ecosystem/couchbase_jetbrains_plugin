32e887c701afee9011cd217fa05b86673c95ec5e
Denis Rosa <denisrosa@K7YYN4RWW5.local>
2023-10-20 20:49:31 +0200

    ïnitial cblite pr

--- src/main/java/com/couchbase/intellij/tree/iq/ChatGptHandler.java
+++ src/main/java/com/couchbase/intellij/tree/iq/ChatGptHandler.java
@@ -1,175 +1,166 @@
-/*
- * Copyright (c) 2023 Mariusz Bernacki <consulting@didalgo.com>
- * SPDX-License-Identifier: Apache-2.0
- */
 package com.couchbase.intellij.tree.iq;
 
-import com.couchbase.intellij.tree.iq.chat.ChatMessageEvent;
-import com.couchbase.intellij.tree.iq.chat.ChatMessageListener;
-import com.couchbase.intellij.tree.iq.chat.ConversationContext;
-import com.intellij.openapi.diagnostic.Logger;
-import com.theokanning.openai.completion.chat.*;
-import io.reactivex.Flowable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import org.apache.commons.lang3.StringUtils;
-import org.reactivestreams.Subscription;
+import com.couchbase.intellij.tree.iq.core.builder.OfficialBuilder;
+import com.couchbase.intellij.tree.iq.core.parser.OfficialParser;
+import com.couchbase.intellij.tree.iq.settings.OpenAISettingsState;
+import com.couchbase.intellij.tree.iq.ui.CouchbaseIQPanel;
+import com.couchbase.intellij.tree.iq.ui.MessageComponent;
+import com.couchbase.intellij.tree.iq.util.StringUtil;
+import com.intellij.openapi.project.Project;
+import okhttp3.*;
+import okhttp3.internal.http2.StreamResetException;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import okhttp3.sse.EventSources;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.net.Proxy;
+import java.nio.charset.StandardCharsets;
+import java.util.Stack;
 import java.util.concurrent.TimeUnit;
 
-public class ChatGptHandler {
 
-    private static final Logger LOG = Logger.getInstance(ChatGptHandler.class);
+public class ChatGPTHandler extends AbstractHandler {
 
-    private static final String TEST_MARKDOWN = """
-            [Selected code from com/didalgo/intellij/chatgpt/ChatGptHandler.java]
-            ```properties
-            # Gradle Releases -> https://github.com/gradle/gradle/releases
-            gradleVersion = 7.6
-            extraCode = <body lang="pl"><p>Line&nbsp;3 &copy; &lt;br&gt;</p></body>
-            
-            # IntelliJ Platform Artifacts Repositories
-            # -> https://www.jetbrains.org/intellij/sdk/docs/reference_guide/intellij_artifacts.html
-            pluginGroup = com.didalgo.chatgpt
-            pluginName = ChatGPT Integration
-            pluginSinceBuild = 222.2680.4
-            pluginUntilBuild = 231.*
-            pluginVersion = 0.1.15-222.231
-            
-            # IntelliJ Platform Properties -> https://github.com/JetBrains/gradle-intellij-plugin#intellij-platform-properties
-            # 'IC' - IntelliJ IDEA Community Edition.
-            # 'IU' - IntelliJ IDEA Ultimate Edition.
-            # 'CL' - CLion.
-            # 'PY' - PyCharm Professional Edition.
-            # 'PC' - PyCharm Community Edition.
-            # 'RD' - Rider.
-            # 'GO' - GoLand.
-            # 'JPS' - JPS-only.
-            platformDownloadSources = true
-            platformPlugins = org.intellij.plugins.markdown
-            platformType = IC
-            platformVersion = 2023.1
-            
-            # Opt-out flag for bundling Kotlin standard library.
-            # See https://plugins.jetbrains.com/docs/intellij/kotlin.html#kotlin-standard-library for details.
-            # suppress inspection "UnusedProperty"
-            kotlin.stdlib.default.dependency = false
-            
-            # Java language level used to compile sources and to generate the files for - Java 11 is required since 2020.3
-            javaVersion = 17
-            
-            org.gradle.jvmargs = '-Dfile.encoding=UTF-8'
-            ```
-            """.repeat(1).strip();
+    private static final Logger LOG = LoggerFactory.getLogger(ChatGPTHandler.class);
+    private final Stack<String> gpt35Stack = new Stack<>();
+    private Project myProject;
 
-    private Flowable<ChatCompletionChunk> streamTestChatCompletion(String markdown) {
-        if (markdown == null || markdown.isEmpty()) {
-            throw new IllegalArgumentException("Hej, ziomeczku, nie możesz przekazać pustego lub nullowego markdownu. Co próbujesz osiągnąć?");
-        }
+    public EventSource handle(CouchbaseIQPanel couchbaseIQPanel, MessageComponent component, String question) {
+        myProject = couchbaseIQPanel.getProject();
+        RequestProvider provider = new RequestProvider().create(couchbaseIQPanel, question);
+        try {
+            Request request = new Request.Builder()
+                    .url(provider.getUrl())
+                    .headers(Headers.of(provider.getHeader()))
+                    .post(RequestBody.create(provider.getData().getBytes(StandardCharsets.UTF_8),
+                            MediaType.parse("application/json")))
+                    .build();
+            OpenAISettingsState instance = OpenAISettingsState.getInstance();
+            OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                    .connectTimeout(Integer.parseInt(instance.connectionTimeout), TimeUnit.MILLISECONDS)
+                    .readTimeout(Integer.parseInt(instance.readTimeout), TimeUnit.MILLISECONDS);
+            builder.hostnameVerifier(getHostNameVerifier());
+            builder.sslSocketFactory(getSslContext().getSocketFactory(), (X509TrustManager) getTrustAllManager());
+            if (instance.enableProxy) {
+                Proxy proxy = getProxy();
+                builder.proxy(proxy);
+            }
+            if (instance.enableProxyAuth) {
+                Authenticator proxyAuth = getProxyAuth();
+                builder.proxyAuthenticator(proxyAuth);
+            }
 
-        return Flowable.range(0, markdown.length())
-                .buffer(5)
-                .concatMap(indices -> {
-                    int startIndex = indices.get(0);
-                    int endIndex = indices.get(indices.size() - 1) + 1;
-                    String chunk = markdown.substring(startIndex, endIndex);
-                    ChatCompletionChunk completionChunk = new ChatCompletionChunk();
-                    List<ChatCompletionChoice> choices = new ArrayList<>();
-                        ChatCompletionChoice choice = new ChatCompletionChoice();
-                        choice.setIndex(0);
-                        choice.setMessage(new ChatMessage(ChatMessageRole.ASSISTANT.value(), chunk));
-                        choices.add(choice);
-                    completionChunk.setChoices(choices);
-                    return Flowable.just(completionChunk)
-                            .delay(25, TimeUnit.MILLISECONDS, Schedulers.computation())
-                            .observeOn(Schedulers.computation());
-                })
-                .observeOn(Schedulers.computation());
-    }
+            OkHttpClient httpClient = builder.build();
+            EventSourceListener listener = new EventSourceListener() {
 
-    public Flowable<?> handle(ConversationContext ctx, ChatMessageEvent.Initiating event, ChatMessageListener listener) {
-        var openAiService = OpenAIServiceHolder.getOpenAiService(ctx.getModelPage());
-        var flowHandler = new ChatCompletionHandler(listener);
-        var request = event.getRequest().orElseThrow(() -> new IllegalArgumentException("ChatCompletionRequest is required"));
+                boolean handler = false;
 
-        if (Boolean.TRUE.equals(request.getStream())) {
-            return openAiService.streamChatCompletion(request)
-            //return streamTestChatCompletion(TEST_MARKDOWN)
-                    .doOnSubscribe(flowHandler.onSubscribe(event))
-                    .doOnError(flowHandler.onError())
-                    .doOnComplete(flowHandler.onComplete(ctx))
-                    .doOnNext(flowHandler.onNextChunk());
-        } else {
-            return Flowable.fromCallable(() -> openAiService.createChatCompletion(request))
-                    .doOnSubscribe(flowHandler.onSubscribe(event))
-                    .doOnError(flowHandler.onError())
-                    .doOnComplete(flowHandler.onComplete(ctx))
-                    .doOnNext(flowHandler.onNext());
-        }
-    }
-
-    static class ChatCompletionHandler {
-        private final ChatMessageListener listener;
-        private final SortedMap<Integer, StringBuffer> partialResponseChoices;
-        private volatile ChatMessageEvent.Started event;
-
-        public ChatCompletionHandler(ChatMessageListener listener) {
-            this.listener = listener;
-            this.partialResponseChoices = Collections.synchronizedSortedMap(new TreeMap<>());
-        }
-
-        public Consumer<Subscription> onSubscribe(ChatMessageEvent.Initiating event) {
-            return subscription -> {
-                listener.exchangeStarted(this.event = event.started(subscription));
-            };
-        }
-
-        public Action onComplete(ConversationContext ctx) {
-            return () -> {
-                var assistantMessages = toMessages(partialResponseChoices);
-                if (!assistantMessages.isEmpty()) {
-                    ctx.addChatMessage(assistantMessages.get(0));
+                @Override
+                public void onOpen(@NotNull EventSource eventSource, @NotNull Response response) {
+                    LOG.info("ChatGPT: conversation open. Url = {}",eventSource.request().url());
                 }
-                listener.responseArrived(event.responseArrived(assistantMessages));
-            };
-        }
 
-        public Consumer<ChatCompletionChunk> onNextChunk() {
-            return chunk -> {
-                if (!chunk.getChoices().isEmpty()) {
-                    listener.responseArriving(event.responseArriving(chunk, formResponse(chunk.getChoices())));
+                @Override
+                public void onClosed(@NotNull EventSource eventSource) {
+                    LOG.info("ChatGPT: conversation close. Url = {}",eventSource.request().url());
+                    if (!handler) {
+                        component.setContent("Connection to remote server failed. There are usually several reasons for this:<br />1. Request too frequently, please try again later.<br />2. It may be necessary to set up a proxy to request.");
+                    }
+                    couchbaseIQPanel.aroundRequest(false);
+                    component.scrollToBottom();
+                    couchbaseIQPanel.getExecutorService().shutdown();
+                }
+
+                @Override
+                public void onEvent(@NotNull EventSource eventSource, @Nullable String id, @Nullable String type, @NotNull String data) {
+                    handler = true;
+                    if (StringUtil.isEmpty(data)) {
+                        return;
+                    }
+                    if (data.contains("[DONE]")) {
+                        return;
+                    }
+                    if (couchbaseIQPanel.isChatGPTModel() && !data.contains("message")) {
+                        return;
+                    }
+                    try {
+                        OfficialParser.ParseResult parseResult;
+                        if (couchbaseIQPanel.isChatGPTModel()) {
+                            parseResult = OfficialParser.
+                                    parseChatGPT(myProject, component, data);
+                        } else {
+                            parseResult = OfficialParser.
+                                    parseGPT35TurboWithStream(component, data);
+                        }
+                        if (parseResult == null) {
+                            return;
+                        }
+                        if (!couchbaseIQPanel.isChatGPTModel()) {
+                            if (data.contains("\"finish_reason\":\"stop\"")) {
+                                couchbaseIQPanel.getContentPanel().getMessages().add(OfficialBuilder.assistantMessage(gpt35Stack.pop()));
+                                gpt35Stack.clear();
+                            } else {
+                                gpt35Stack.push(parseResult.getSource());
+                            }
+                        }
+                        // Copy action only needed source content
+                        component.setSourceContent(parseResult.getSource());
+                        component.setContent(parseResult.getHtml());
+                    } catch (Exception e) {
+                        LOG.error("ChatGPT: Parse response error, e={}, message={}", e, e.getMessage());
+                        component.setContent(e.getMessage());
+                    } finally {
+                        couchbaseIQPanel.getExecutorService().shutdown();
+                    }
+                }
+
+                @Override
+                public void onFailure(@NotNull EventSource eventSource, @Nullable Throwable t, @Nullable Response response) {
+                    if (t != null) {
+                        if (t instanceof StreamResetException) {
+                            LOG.info("ChatGPT: Request failure, throwable StreamResetException, cause: {}", t.getMessage());
+                            component.setContent("Request failure, cause: " + t.getMessage());
+                            couchbaseIQPanel.aroundRequest(false);
+                            t.printStackTrace();
+                            return;
+                        }
+                        LOG.info("ChatGPT: conversation failure. Url={}, response={}, errorMessage={}",eventSource.request().url(), response, t.getMessage());
+                        component.setContent("Response failure, cause: " + t.getMessage() + ", please try again. <br><br> Tips: if proxy is enabled, please check if the proxy server is working.");
+                        couchbaseIQPanel.aroundRequest(false);
+                        t.printStackTrace();
+                    } else {
+                        String responseString = "";
+                        if (response != null) {
+                            try {
+                                responseString = response.body().string();
+                            } catch (IOException e) {
+                                couchbaseIQPanel.aroundRequest(false);
+                                LOG.error("ChatGPT: parse response error, cause: {}", e.getMessage());
+                                component.setContent("Response failure, cause: " + e.getMessage());
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        LOG.info("ChatGPT: conversation failure. Url={}, response={}",eventSource.request().url(), response);
+                        component.setContent("Response failure, please try again. Error message: " + responseString);
+                    }
+                    couchbaseIQPanel.aroundRequest(false);
+                    component.scrollToBottom();
+                    couchbaseIQPanel.getExecutorService().shutdown();
                 }
             };
+            EventSource.Factory factory = EventSources.createFactory(httpClient);
+            return factory.newEventSource(request, listener);
+        } catch (Exception e) {
+            LOG.error("ChatGPT handle Exception, error: {}", e.getMessage());
+        } finally {
+            couchbaseIQPanel.getExecutorService().shutdown();
         }
-
-        public Consumer<ChatCompletionResult> onNext() {
-            return result -> {
-                if (!result.getChoices().isEmpty()) {
-                    listener.responseArrived(event.responseArrived(formResponse(result.getChoices())));
-                }
-            };
-        }
-
-        public Consumer<Throwable> onError() {
-            return cause -> {
-                listener.exchangeFailed(event.failed(cause));
-            };
-        }
-
-        private List<ChatMessage> formResponse(List<ChatCompletionChoice> choices) {
-            choices.forEach(choice -> {
-                partialResponseChoices.computeIfAbsent(choice.getIndex(), __ -> new StringBuffer())
-                        .append(StringUtils.defaultIfEmpty(choice.getMessage().getContent(), ""));
-            });
-            return toMessages(partialResponseChoices);
-        }
-
-        private List<ChatMessage> toMessages(SortedMap<Integer, StringBuffer> partialResponseChoices) {
-            List<ChatMessage> responseChoices = new ArrayList<>(partialResponseChoices.size());
-            partialResponseChoices.forEach((key, value) -> responseChoices.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), value.toString())));
-            return responseChoices;
-        }
+        return null;
     }
 }
