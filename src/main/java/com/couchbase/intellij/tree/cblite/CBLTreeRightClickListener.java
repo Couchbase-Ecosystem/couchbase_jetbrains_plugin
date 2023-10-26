ed36bfa0fc4ad5dcfe3adde6ad25211397840596
Denis Rosa <denisrosa@K7YYN4RWW5.local>
2023-10-26 23:06:17 +0200

    listing documents

--- src/main/java/com/couchbase/intellij/tree/cblite/CBLTreeRightClickListener.java
+++ src/main/java/com/couchbase/intellij/tree/cblite/CBLTreeRightClickListener.java
@@ -1,14 +1,18 @@
 package com.couchbase.intellij.tree.cblite;
 
+import com.couchbase.intellij.database.ActiveCluster;
 import com.couchbase.intellij.tree.cblite.nodes.CBLCollectionNodeDescriptor;
 import com.couchbase.intellij.tree.cblite.nodes.CBLDatabaseNodeDescriptor;
+import com.couchbase.intellij.tree.cblite.nodes.CBLFileNodeDescriptor;
 import com.couchbase.intellij.workbench.Log;
 import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Document;
 import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.JBPopup;
@@ -19,6 +23,7 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import java.awt.event.MouseEvent;
 
 public class CBLTreeRightClickListener {
@@ -32,7 +37,54 @@ public class CBLTreeRightClickListener {
             handleDatabase(project, e, clickedNode, (CBLDatabaseNodeDescriptor) userObject, tree);
         } else if (userObject instanceof CBLCollectionNodeDescriptor) {
             handleCollection(project, e, clickedNode, (CBLCollectionNodeDescriptor) userObject, tree);
+        } else if (userObject instanceof CBLFileNodeDescriptor) {
+            handleDocument(project, e, clickedNode, (CBLFileNodeDescriptor) userObject, tree);
         }
+    }
+
+    private static void handleDocument(Project project, MouseEvent e, DefaultMutableTreeNode clickedNode, CBLFileNodeDescriptor userObject, Tree tree) {
+        DefaultActionGroup actionGroup = new DefaultActionGroup();
+        AnAction deleteDocument = new AnAction("Delete Document") {
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                int result = Messages.showYesNoDialog("<html>Are you sure you want to delete the document <strong>" + userObject.getId() + "</strong>?</html>", "Delete Document", Messages.getQuestionIcon());
+                if (result != Messages.YES) {
+                    return;
+                }
+
+                try {
+                    Document document =  ActiveCBLDatabase.getInstance().getDatabase().
+                            getScope(userObject.getScope())
+                            .getCollection(userObject.getCollection()).getDocument(userObject.getId());
+
+                    ActiveCBLDatabase.getInstance().getDatabase().
+                            getScope(userObject.getScope())
+                            .getCollection(userObject.getCollection()).delete(document);
+
+                    if (userObject.getVirtualFile() != null) {
+                        try {
+                            FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
+                            fileEditorManager.closeFile(userObject.getVirtualFile());
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                            Log.debug("Could not close the file", ex);
+                        }
+                    }
+
+                    DefaultMutableTreeNode parentNode = (DefaultMutableTreeNode) clickedNode.getParent();
+                    if (parentNode != null) {
+                        ((DefaultTreeModel) tree.getModel()).removeNodeFromParent(clickedNode);
+                    }
+                    ((DefaultTreeModel) tree.getModel()).nodeStructureChanged(parentNode);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    Log.error("An error occurred while trying to delete the document " + userObject.getId(), ex);
+                    Messages.showErrorDialog("Could not delete the document. Please check the logs for more.", "Couchbase Plugin Error");
+                }
+            }
+        };
+        actionGroup.add(deleteDocument);
+        showPopup(e, tree, actionGroup);
     }
 
     private static void handleCollection(Project project, MouseEvent e, DefaultMutableTreeNode clickedNode, CBLCollectionNodeDescriptor userObject, Tree tree) {
@@ -55,14 +107,6 @@ public class CBLTreeRightClickListener {
             }
         };
         actionGroup.add(createDocument);
-
-        AnAction deleteDocument = new AnAction("Delete Document") {
-            @Override
-            public void actionPerformed(@NotNull AnActionEvent e) {
-                //TODO
-            }
-        };
-        actionGroup.add(deleteDocument);
 
         showPopup(e, tree, actionGroup);
     }
